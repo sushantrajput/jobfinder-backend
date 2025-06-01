dc151118d91c7250d1e0e5aeed9ef487f894eec7
sushantsinghrajput <skrajsushant698@gmail.com>
2025-06-01 17:45:58 +0530

    postgres

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysql = require("mysql2");
+const { Pool } = require("pg");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 
@@ -9,22 +9,22 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-// DB connection using promises
-const db = mysql.createPool({
-  host: "localhost", 
-  user: "root",
-  password: "", 
-  database: "jobfinder_db",
-}).promise();
+
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL ,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
+});
 
 // Test DB connection
 db.query("SELECT 1")
-  .then(() => console.log("✅ Connected to MySQL database"))
+  .then(() => console.log("✅ Connected to PostgreSQL database"))
   .catch((err) => console.error("❌ DB connection failed:", err));
 
 // Signup Route
 app.post("/signup", async (req, res) => {
-  const {name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
     return res.status(400).json({ message: "All fields are required" });
@@ -32,18 +32,17 @@ app.post("/signup", async (req, res) => {
 
   try {
     // Check if email already exists
-    const [existingUser] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
-    if (existingUser.length > 0) {
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+    if (result.rows.length > 0) {
       return res.status(409).json({ message: "Email already registered" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    await db.query("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [
-      name,
-      email,
-      hashedPassword,
-    ]);
+    await db.query(
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)",
+      [name, email, hashedPassword]
+    );
 
     res.status(201).json({ message: "User registered successfully" });
   } catch (err) {
@@ -61,13 +60,13 @@ app.post("/login", async (req, res) => {
   }
 
   try {
-    const [rows] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
 
-    if (rows.length === 0) {
+    if (result.rows.length === 0) {
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
-    const user = rows[0];
+    const user = result.rows[0];
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
