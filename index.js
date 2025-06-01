6cf58638e157f4e40563f337e0ddca49c45f07db
sushantsinghrajput <skrajsushant698@gmail.com>
2025-06-01 19:08:30 +0530

    postgre-8

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bcrypt = require("bcrypt");
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 
 // CORS setup
 app.use(cors({
